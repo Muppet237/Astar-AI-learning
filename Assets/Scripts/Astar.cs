df1744312a79aa114de00867537dfe73bfe7ea23
LQMemer <42935807+Muppet237@users.noreply.github.com>
2022-07-05 22:19:16 +0200

    Bare bone

---
+++ Assets/Scripts/Astar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astar:MonoBehaviour {
+
+    public Vector3 target;
+    public string[] blockers;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
--- Assets/Scripts/Personal AI/PAI.cs
+++ Assets/Scripts/Personal AI/PAI.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PAI:MonoBehaviour {
+
+    public GameObject target;
+    public float visibleRange;
+
     void Start() {
 
     }
 
     void Update() {
 
+
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+
+        if(distanceToTarget > visibleRange)
+            return;
+        
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, target.transform.position, out hit, distanceToTarget)) {
+            if(hit.collider.CompareTag("Player")) {
+                
+            }   
+        }
     }
 }

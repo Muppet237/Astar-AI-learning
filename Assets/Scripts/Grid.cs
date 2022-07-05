602099197aa295325bb265c2052b9080c377358f
LQMemer <42935807+Muppet237@users.noreply.github.com>
2022-07-05 21:21:22 +0200

    Added a bunch of scripts and stuff

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid:MonoBehaviour {
+
+    public LayerMask unwalkableMask;
+
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, gridWorldSize);    
+    }
+}

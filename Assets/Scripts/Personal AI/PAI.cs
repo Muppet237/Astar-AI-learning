da9ef5a8ced045bf135e8faa4e51b745253cac7c
LQMemer <42935807+Muppet237@users.noreply.github.com>
2022-07-06 20:52:23 +0200

    Movement and detection update
    Got stuck with some stupid rotation math, a simple eulerAngles seems to have fixed it

--- Assets/Scripts/Personal AI/PAI.cs
+++ Assets/Scripts/Personal AI/PAI.cs
@@ -8,27 +8,49 @@ public class PAI:MonoBehaviour {
     public float visibleRange, movementSpeed;
 
     Rigidbody rgbd;
+    Vector3 cornerOffset;
+    bool lockedOnTarget = true, foundCorner;
 
     void Start() {
         rgbd = GetComponent<Rigidbody>();
     }
 
     void Update() {
+
         float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
         if(distanceToTarget > visibleRange)
             return;
 
-        transform.LookAt(target.transform.position);
+        if(lockedOnTarget)
+            transform.LookAt(target.transform.position);
 
-        //Debug.DrawRay(transform.position, transform.forward * distanceToTarget, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * distanceToTarget, Color.red);
         RaycastHit hit;
         if(Physics.Raycast(transform.position, transform.forward, out hit, distanceToTarget)) {
+            Debug.Log(hit.collider.tag);
             if(!hit.collider.CompareTag("Player")) {
                 rgbd.velocity = Vector3.zero;
+                lockedOnTarget = false;
+                FindCorner();
                 return;
             }
 
+            lockedOnTarget = true;
             rgbd.velocity = transform.forward * movementSpeed;
+        }
+    }
+
+    void FindCorner() {
+        RaycastHit hit;
+
+        float holdAngle = transform.eulerAngles.y;
+        for(float i = 0; i <= 45; i += 0.1f) {
+            transform.rotation = Quaternion.Euler(0, holdAngle + i, 0);
+            if(Physics.Raycast(transform.position, transform.forward, out hit)) {
+                if(hit.collider.CompareTag("Player"))
+                    lockedOnTarget = true;
+
+            }
         }
     }
 

1a0fb638d0a1e92fec7ae0d5cb7eaba224aa1af4
LQMemer <42935807+Muppet237@users.noreply.github.com>
2022-07-07 21:25:56 +0200

    Added corner checker
    It works a little bit, needs work though, only checks corners on to the right.

--- Assets/Scripts/Personal AI/PAI.cs
+++ Assets/Scripts/Personal AI/PAI.cs
@@ -4,57 +4,101 @@ using UnityEngine;
 
 public class PAI:MonoBehaviour {
 
-    public GameObject target;
-    public float visibleRange, movementSpeed;
+    public GameObject target, pointer;
+    public float visibleRange, movementSpeed, bodyToWall = 1;
 
     Rigidbody rgbd;
-    Vector3 cornerOffset;
-    bool lockedOnTarget = true, foundCorner;
+    Vector3 cornerOffset, tempVector;
+    bool lockedOnPlayer = true, foundCorner, findPlayer;
+    float distanceToTarget;
 
     void Start() {
         rgbd = GetComponent<Rigidbody>();
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit)) {
+            tempVector = hit.point;
+        }
     }
 
     void Update() {
+        Debug.DrawRay(transform.position, transform.forward * distanceToTarget, Color.red);
 
-        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
         if(distanceToTarget > visibleRange)
             return;
 
-        if(lockedOnTarget)
-            transform.LookAt(target.transform.position);
+        if(findPlayer) {
+            FindPlayer();
+            return;
+        }
 
-        Debug.DrawRay(transform.position, transform.forward * distanceToTarget, Color.red);
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, distanceToTarget)) {
-            Debug.Log(hit.collider.tag);
-            if(!hit.collider.CompareTag("Player")) {
-                rgbd.velocity = Vector3.zero;
-                lockedOnTarget = false;
-                FindCorner();
+        if(foundCorner) {
+            if(Vector3.Distance(cornerOffset, transform.position) < 0.1f) {
+                foundCorner = false;
+                findPlayer = true;
                 return;
             }
 
-            lockedOnTarget = true;
-            rgbd.velocity = transform.forward * movementSpeed;
+            MoveTowards(cornerOffset);
+            return;
         }
+
+        if(lockedOnPlayer)
+            StandardMovement();
+    }
+
+    void FindPlayer() {
+        transform.LookAt(target.transform.position);
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, distanceToTarget)) {
+            if(hit.collider.CompareTag("Player")) {
+                lockedOnPlayer = true;
+                findPlayer = false;
+                return;
+            }
+        }
+        tempVector = hit.point;
+        findPlayer = false;
+        FindCorner();
+    }
+
+    void StandardMovement() {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, distanceToTarget)) {
+            if(!hit.collider.CompareTag("Player")) {
+                rgbd.velocity = Vector3.zero;
+                tempVector = hit.point;
+                lockedOnPlayer = false;
+                FindCorner();
+                return;
+            }
+        }
+        MoveTowards(target.transform.position);
     }
 
     void FindCorner() {
-        RaycastHit hit;
-
         float holdAngle = transform.eulerAngles.y;
-        for(float i = 0; i <= 45; i += 0.1f) {
+        
+        RaycastHit hitFind;
+        for(float i = 1; i <= 90; i += 0.25f) {
             transform.rotation = Quaternion.Euler(0, holdAngle + i, 0);
-            if(Physics.Raycast(transform.position, transform.forward, out hit)) {
-                if(hit.collider.CompareTag("Player"))
-                    lockedOnTarget = true;
-
+            if(Physics.Raycast(transform.position, transform.forward, out hitFind)) {
+                if(Vector3.Distance(tempVector, hitFind.point) > 0.1) {
+                    cornerOffset = tempVector + Vector3.right * bodyToWall;
+                    foundCorner = true;
+                    Debug.Log(cornerOffset);
+                    //Instantiate(pointer, cornerOffset, Quaternion.identity);
+                    break;
+                }
+                tempVector = hitFind.point;
             }
         }
     }
 
-    void MoveTowards() {
-
+    void MoveTowards(Vector3 _target) {
+        //Debug.Log(_target);
+        transform.LookAt(_target);
+        rgbd.velocity = transform.forward * movementSpeed;
     }
 }

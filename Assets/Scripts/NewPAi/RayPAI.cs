de5df11f204e0f2457c5e510afd4ca947df733f1
LQMemer <42935807+Muppet237@users.noreply.github.com>
2022-07-14 04:40:48 +0200

    Progress?
    Sure works better than the last time. Buggy af though. Jumpin all over the place.

--- Assets/Scripts/NewPAi/RayPAI.cs
+++ Assets/Scripts/NewPAi/RayPAI.cs
@@ -19,24 +19,28 @@ public class RayPAI:MonoBehaviour {
     void Update() {
         transform.LookAt(target.transform.position);
         Vector3 forcedDirection = target.transform.position - transform.position;
-        
 
         int angle = 0;
         for(int i = 0; i < 8; i++) {
             float distanceToPlayer = Vector3.Distance(target.transform.position, transform.position);
             rayDistance[i] = Mathf.Clamp(Vector3.Distance(transform.position + Quaternion.Euler(0, angle, 0) * transform.forward * distanceToPlayer, target.transform.position), 0, maxDistance);
-            maxDistance -= rayDistance[i];
+            maxDistance -= rayDistance[i] * 0.5f;
 
             Vector3 rayDirection = Quaternion.Euler(0, angle, 0) * transform.forward;
 
             Debug.DrawRay(transform.position, rayDirection * maxDistance, Color.red);
-            RaycastHit hit;
-            if(Physics.Raycast(transform.position, Quaternion.Euler(0, angle, 0) * transform.forward * maxDistance, out hit)) {
-                if(!hit.collider.CompareTag("Player")) {
-                       
-                }
 
-                rgbd.velocity = forcedDirection.normalized * speed;
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, rayDirection * maxDistance, out hit)) {
+                if(!hit.collider.CompareTag("Player")) {
+                    if(Vector3.Distance(transform.position, hit.point) < 3) {
+                        rgbd.velocity = (Vector3.one + -rayDirection.normalized * pushSpeed) + forcedDirection.normalized * speed;
+                    } else {
+                        rgbd.velocity = forcedDirection.normalized * speed;
+                    }
+                } else {
+                    rgbd.velocity = forcedDirection.normalized * speed;
+                }
             }
             angle += 45;
             maxDistance = 10;

9b3ac7e785c04b58260af76abb6c3dbe5251a195
LQMemer <42935807+Muppet237@users.noreply.github.com>
2022-07-14 04:26:02 +0200

    Update?

--- Assets/Scripts/NewPAi/RayPAI.cs
+++ Assets/Scripts/NewPAi/RayPAI.cs
@@ -5,33 +5,41 @@ using UnityEngine;
 public class RayPAI:MonoBehaviour {
 
     public GameObject target;
-    public float speed;
+    public float speed, pushSpeed;
 
-    float[] distanceToPlayer = new float[8];
+    float[] rayDistance = new float[8];
     float maxDistance = 10;
 
     Rigidbody rgbd;
 
     void Start() {
         rgbd = GetComponent<Rigidbody>();
-
     }
 
     void Update() {
         transform.LookAt(target.transform.position);
         Vector3 forcedDirection = target.transform.position - transform.position;
-        rgbd.velocity = forcedDirection * speed;
+        
 
         int angle = 0;
         for(int i = 0; i < 8; i++) {
-            float test = Vector3.Distance(target.transform.position, transform.position);
-            distanceToPlayer[i] = Mathf.Clamp(Vector3.Distance(transform.position + Quaternion.Euler(0, angle, 0) * transform.forward * test, target.transform.position), 0, maxDistance);
-            maxDistance -= distanceToPlayer[i];
+            float distanceToPlayer = Vector3.Distance(target.transform.position, transform.position);
+            rayDistance[i] = Mathf.Clamp(Vector3.Distance(transform.position + Quaternion.Euler(0, angle, 0) * transform.forward * distanceToPlayer, target.transform.position), 0, maxDistance);
+            maxDistance -= rayDistance[i];
 
-            Debug.DrawRay(transform.position, Quaternion.Euler(0, angle, 0) * transform.forward * maxDistance, Color.red);
+            Vector3 rayDirection = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+            Debug.DrawRay(transform.position, rayDirection * maxDistance, Color.red);
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, Quaternion.Euler(0, angle, 0) * transform.forward * maxDistance, out hit)) {
+                if(!hit.collider.CompareTag("Player")) {
+                       
+                }
+
+                rgbd.velocity = forcedDirection.normalized * speed;
+            }
             angle += 45;
-            maxDistance = 5;
+            maxDistance = 10;
         }
-
     }
 }
